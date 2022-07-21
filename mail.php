2b35aab95e9fef94ed2034aa9102d7e6b668569a
Mano <mano.26q@gmail.com>
2022-07-21 21:47:25 +0530

    Updated

---
+++ mail.php
@@ -0,0 +1,17 @@
+<?php
+//get data from form  
+
+$name = $_POST['name'];
+$email= $_POST['emailname'];
+$message= $_POST['message'];
+$to = "mano.astronaut03@gmail.com";
+$subject = "Mail From website";
+$txt ="Name = ". $name . "\r\n  Email = " . $email . "\r\n Message =" . $message;
+$headers = "From: noreply@codeconia.com" . "\r\n" .
+"CC: somebodyelse@example.com";
+if($email!=NULL){
+    mail($to,$subject,$txt,$headers);
+}
+//redirect
+header("Location:thankyou.html");
+?>
